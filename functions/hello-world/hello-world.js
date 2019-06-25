7c1cd979a649d8f6de7e08889f30ee617274f2b2
Benedicte Raae <raae@bgraphic.no>
2019-06-25 19:00:50 +0200

    Reading and writing encypted notes

--- functions/hello-world/hello-world.js
+++ functions/hello-world/hello-world.js
@@ -1,10 +1,78 @@
-exports.handler = async (event, context) => {
-  console.log(JSON.stringify(event, null, 2))
-  console.log(JSON.stringify(context, null, 2))
+const axios = require("axios")
+const Octokit = require("@octokit/rest")
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+const PARAMS = {
+  owner: "raae",
+  repo: "octokit-notes",
+  committer: {
+    name: "pow",
+    email: "pow@raae.codes",
+  },
+}
+
+const post = async ({ body }) => {
+  let { date, entry } = JSON.parse(body)
+  entry = JSON.stringify(entry, null, 2)
+  const params = {
+    ...PARAMS,
+    path: date,
+    message: "New note",
+    content: Buffer.from(entry).toString("base64"),
+  }
+
   try {
-    const subject = event.queryStringParameters.name || "World"
-    return { statusCode: 200, body: `Hello ${subject}` }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+    const existingFile = await octokit.repos.getContents(params)
+    params.message = "Updated note"
+    params.sha = existingFile.data.sha
+  } catch (error) {
+    // No existing file
+  }
+
+  try {
+    await octokit.repos.createOrUpdateFile(params)
+    return {
+      statusCode: 200,
+      body: `${params.repo}/${params.path} added/updated`,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+const get = async () => {
+  try {
+    const params = {
+      ...PARAMS,
+      path: "/",
+    }
+    const response = await octokit.repos.getContents(params)
+    const notePromises = response.data.map(async file => {
+      const { data } = await axios.get(file.download_url)
+      return {
+        date: file.name,
+        entry: data,
+      }
+    })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(await Promise.all(notePromises), null, 2),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+exports.handler = async event => {
+  switch (event.httpMethod) {
+    case "POST":
+      return post(event)
+    case "GET":
+      return get(event)
+    default:
+      break
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,134 @@
-import React, { useRef } from "react"
+import React, { useRef, useState, useEffect } from "react"
+import localforage from "localforage"
 import axios from "axios"
 
-export default () => {
+const ALGORITHM = "AES-GCM"
+const KEY_LENGTH = 256
+
+const encode = text => {
+  return new TextEncoder().encode(text)
+}
+
+const decode = buffer => {
+  return new TextDecoder("utf-8").decode(buffer)
+}
+
+const serialize = buffer => {
+  return new Uint8Array(buffer).toString()
+}
+
+const deserialize = string => {
+  return new Uint8Array(string.split(","))
+}
+
+const generateKey = async () => {
+  console.log("Generate key")
+  return await window.crypto.subtle.generateKey(
+    { name: ALGORITHM, length: KEY_LENGTH },
+    true,
+    ["encrypt", "decrypt"]
+  )
+}
+
+const initKey = async () => {
+  try {
+    let key = await localforage.getItem("cryptokey")
+    if (!key) {
+      key = await generateKey()
+    }
+    return await localforage.setItem("cryptokey", key)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const generateNonce = () => {
+  return window.crypto.getRandomValues(new Uint8Array(12))
+}
+
+const encrypt = async (text, key, iv) => {
+  const encryptedBuffer = await window.crypto.subtle.encrypt(
+    { name: ALGORITHM, iv: iv },
+    key,
+    encode(text)
+  )
+
+  return encryptedBuffer
+}
+
+const decrypt = async (cypherBuffer, key, iv) => {
+  const decryptedBuffer = await window.crypto.subtle.decrypt(
+    { name: ALGORITHM, iv: iv },
+    key,
+    cypherBuffer
+  )
+
+  return decode(decryptedBuffer)
+}
+
+const fetchNotes = async key => {
+  const response = await axios.get("/.netlify/functions/hello-world")
+  const notePromises = response.data.map(async note => {
+    note.entry = await decrypt(
+      deserialize(note.entry.cypher),
+      key,
+      deserialize(note.entry.iv)
+    )
+    return note
+  })
+  return await Promise.all(notePromises)
+}
+
+const saveNote = async ({ date, entry }, key) => {
+  const iv = generateNonce()
+
+  const note = {
+    date: date,
+    entry: {
+      cypher: serialize(await encrypt(entry, key, iv)),
+      iv: serialize(iv),
+    },
+  }
+
+  return await axios.post("/.netlify/functions/hello-world", note)
+}
+
+const IndexPage = () => {
+  const [key, setKey] = useState()
+  const [notes, setNotes] = useState([])
+  const [saving, setSaving] = useState(false)
   const dateEl = useRef(null)
   const entryEl = useRef(null)
   const formEl = useRef(null)
 
-  const onSubmit = event => {
+  useEffect(() => {
+    const initData = async () => {
+      const key = await initKey()
+      setKey(key)
+      setNotes(await fetchNotes(key))
+    }
+
+    initData()
+  }, [])
+
+  const onSubmit = async event => {
     event.preventDefault(event)
-    const data = {
+
+    const note = {
       date: dateEl.current.value,
       entry: entryEl.current.value,
     }
 
-    axios.post("/.netlify/functions/hello-world", data)
-
-    formEl.current.reset()
+    try {
+      setSaving(true)
+      await saveNote(note, key)
+      await setNotes(await fetchNotes(key))
+      setSaving(false)
+      formEl.current.reset()
+    } catch (error) {
+      setSaving(false)
+      console.error(error)
+    }
   }
 
   return (
@@ -27,10 +140,24 @@ export default () => {
         <br />
         Notes:
         <br />
-        <input ref={entryEl} type="text" name="entry" />
+        <textarea ref={entryEl} type="text" name="entry" />
         <br />
-        <button type="submit">Submit</button>
+        <button disabled={!key || saving} type="submit">
+          Submit
+        </button>
       </form>
+      <div>
+        {notes.map(({ date, entry }) => {
+          return (
+            <div key={date}>
+              <h1>{date}</h1>
+              <p>{entry}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
+
+export default IndexPage
