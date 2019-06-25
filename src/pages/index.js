100967d56d58ff6fee7bf4108f1a652c87ac7be6
Benedicte Raae <raae@bgraphic.no>
2019-06-25 16:08:43 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <form>
+      Date:
+      <br />
+      <input type="date" name="date" />
+      <br />
+      Notes:
+      <br />
+      <input type="text" name="note" />
+    </form>
+  </div>
+)
